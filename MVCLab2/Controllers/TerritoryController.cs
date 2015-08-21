254e8d51927faf8ea6565731ae1bc2d87bfaf598
Karina <casavik@gmail.com>
2015-08-21 00:12:23 -0600

    New model

---
+++ MVCLab2/Controllers/TerritoryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLab2.Servicio;
+
+namespace MVCLab2.Controllers
+{
+    public class TerritoryController : Controller
+    {
+
+        private ServicioClient servicio = new ServicioClient();
+
+        public ActionResult Index()
+        {
+            ViewBag.listCustomerTerritory = servicio.ObtenerClientes();
+
+            return View();
+        }
+
+        public ActionResult Search(FormCollection fc)
+        {
+            string condition = fc["condition"];
+            string keyword = fc["keyword"];
+            if (condition.Equals("byID"))
+                ViewBag.listCustomerTerritory = servicio.ObtenerClientesNombreCliente(keyword);
+            else
+                ViewBag.listCustomerTerritory = servicio.ObtenerClientesNombrePais(keyword);
+
+            return View("Index");
+        }
+    }
+}
---
+++ WcfService1/IServicio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService1
+{
+    [ServiceContract]
+    public interface IServicio
+    {
+        [OperationContract]
+        List<CustomerTerritory> ObtenerClientesNombreCliente(String nombre);
+
+
+        [OperationContract]
+        List<CustomerTerritory> ObtenerClientesNombrePais(String pais);
+
+        [OperationContract]
+        List<CustomerTerritory> ObtenerClientes();
+    }
+}
